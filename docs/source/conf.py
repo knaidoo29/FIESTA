e82bd5a424e5830e25b743db1d43066c317f0de1
Krishna Naidoo <krishna.naidoo.11@ucl.ac.uk>
2025-03-16 17:47:07 +0000

    add logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,13 +1,17 @@
 # Configuration file for the Sphinx documentation builder.
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+
+import vinfo
 
 # -- Project information
 
-project = 'FIESTA'
-copyright = '2021, Krishna Naidoo'
+project = 'FiEsta'
+copyright = '2021-2025, Krishna Naidoo'
 author = 'Krishna Naidoo'
 
-release = '0.0'
-version = '0.0.1'
+version = vinfo.vstr
 
 # -- General configuration
 
@@ -17,7 +21,15 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.mathjax',
+    'sphinx_simplepdf'
 ]
+
+source_suffix = ['.rst', '.md']
+
+# Napoleon settings
+napoleon_numpy_docstring = True
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
@@ -27,9 +39,17 @@ intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
+
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
+
+html_static_path = ["_static"]
+html_theme_options = {
+    "light_logo": "FiEsta_logo_small_white.jpg",
+    "dark_logo": "FiEsta_logo_small_black.jpg",
+}
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
